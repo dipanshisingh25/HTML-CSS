a4570480ff2733114236e1020a4657e8e20150c2
dipanshisingh25 <dipanshisingh25@navgurukul.org>
2025-11-17 21:53:02 -0800

    Create Q1.js

---
+++ Q1.js
@@ -0,0 +1,26 @@
+// Problem Statement:
+// Write a program that takes an integer as input and prints whether it is "Positive", "Negative", or "Zero".
+// Constraints:
+// Do not use any inbuilt functions, methods, or external libraries.
+// Sample Input 1:
+// 15
+// Sample Output 1:
+// Positive
+// Sample Input 2:
+// -3
+// Sample Output 2:
+// Negative
+// Sample Input 3:
+// 0
+// Sample Output 3:
+// Zero
+
+const integer = 13;
+
+if(integer > 0){
+  console.log("Positive");
+}else if(integer < 0){
+  console.log("Negative");
+}else{
+  copnsole.log("Zero");
+}
